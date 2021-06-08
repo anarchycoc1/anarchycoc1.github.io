f6658087169f2d9ddc8518f54cadb2afa58970da
anarchycoc1 <85509576+anarchycoc1@users.noreply.github.com>
2021-06-08 03:04:35 +0300

    12

--- js/script.js
+++ js/script.js
@@ -16,3 +16,20 @@ document.querySelector("#anc15").onclick = function () { alert("Name: . \nTown H
 document.querySelector("#anc16").onclick = function () { alert("Name: . \nTown Hall: . \nLVL: . \nLeague: . \nCWL: ( Атак/Звезд ). "); };
 document.querySelector("#anc17").onclick = function () { alert("Name: . \nTown Hall: . \nLVL: . \nLeague: . \nCWL: ( Атак/Звезд ). "); };
 document.querySelector("#anc18").onclick = function () { alert("Name: . \nTown Hall: . \nLVL: . \nLeague: . \nCWL: ( Атак/Звезд ). "); };
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('menu_active');
+  });
+
+  menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu_active');
+    })
+  })
+})
